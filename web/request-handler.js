015617833de40b768136ed8573487afea4fe30c1
kranrao <k.ranrao@gmail.com>
2015-01-07 17:11:25 -0800

    Passes first two tests - '/' returns index and 'url' returns its archived site

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET'){
+    res.writeHead(200, httpHelpers.headers);
+
+    // In future use url parse to do this correctly
+    if (req.url === '/'){
+      fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data){
+        res.end(data.toString());
+      });
+    } else {
+      fs.readFile(archive.paths.archivedSites + req.url, function(err, data){
+        res.end(data.toString());
+      });
+    }
+  }
+  /*res.end();*/
 };
